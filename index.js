b9b52acac7554dbeb5162e9f01ab0a954ad665c2
bmordan <bernardmordan@gmail.com>
2019-03-18 21:04:46 +0000

    switched on

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+
+const scuberGreetingForFeet = ft => {
+    if (ft <= 400) return 'This one is on me!'
+    if (ft > 2000 && ft < 2500) return 'I will gladly take your thirty bucks.'
+    if (ft > 2500) return 'No can do.'
+}
+
+const ternaryCheckCity = city => city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+
+const switchOnCharmFromTip = tip => {
+    switch (tip) {
+        case 'generous':
+            return 'Thank you so much.'
+        case 'not as generous':
+            return 'Thank you.'
+        case 'thanks for everything':
+            return 'Bye.'
+    }
+}
