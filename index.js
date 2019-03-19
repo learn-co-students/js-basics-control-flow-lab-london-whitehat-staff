23fa08019cd87fa90243b8887fbcd0e3d2411570
Christopher Orrick <christopherorrick@me.com>
2019-03-19 13:38:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someNumber) {
+  let greeting
+  if (someNumber <= 400) {
+    greeting = 'This one is on me!';
+  } else if (someNumber >= 2500) {
+    greeting = 'No can do.';
+  } else {
+    greeting = 'I will gladly take your thirty bucks.';
+  }
+  return greeting
+}
+
+function ternaryCheckCity(someCity) {
+  let greeting
+  switch (someCity) {
+    case 'NYC':
+      greeting = 'Ok, sounds good.';
+      break;
+  
+    default:
+      greeting = 'No go.';
+      break;
+  }
+  
+  return greeting
+}
+
+function switchOnCharmFromTip(someTip) {
+  let greeting
+  switch (someTip) {
+    case 'generous':
+      greeting = 'Thank you so much.';
+      break;
+
+    case 'not as generous':
+      greeting = 'Thank you.';
+      break;
+  
+    default:
+      greeting = 'Bye.';
+      break;
+  }
+  
+  return greeting
+}
